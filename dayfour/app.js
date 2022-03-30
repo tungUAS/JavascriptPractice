7d7579980b6fdf621d978a8a2ebf010db9fd7d15
tungUAS <tung.tt133@gmail.com>
2022-03-30 17:01:50 +0200

    dayfour

---
+++ dayfour/app.js
@@ -0,0 +1,55 @@
+var images = document.querySelectorAll('.image img')
+var prev = document.querySelector('.prev')
+var right = document.querySelector('.right')
+var close = document.querySelector('.close')
+var galleryImg = document.querySelector('.gallery_inner img')
+var gallery = document.querySelector('.gallery')
+
+var currentIndex = 0;
+
+function showGallery(){
+    if(currentIndex == 0){
+        prev.classList.add('hide')
+    }else{
+        prev.classList.remove('hide')
+    }
+
+    if(currentIndex == images.length-1){
+        right.classList.add('hide')
+    }else{
+        right.classList.remove('hide')
+    }
+    galleryImg.src=images[currentIndex].src
+    gallery.classList.add('show')
+}
+
+images.forEach((item,index)=>{
+    item.addEventListener('click',function(){
+        currentIndex = index;
+        showGallery();
+    })
+})
+
+close.addEventListener('click',function(){
+    gallery.classList.remove('show')
+})
+
+document.addEventListener('keydown',function(){
+    if(e.keycode == 27){
+        gallery.classList.remove('show')
+    }
+})
+
+prev.addEventListener('click',function(){
+    if(currentIndex > 0){
+        currentIndex--;
+        showGallery();
+    }
+})
+
+right.addEventListener('click',function(){
+    if(currentIndex < images.length-1){
+        currentIndex--;
+        showGallery();
+    }
+})
